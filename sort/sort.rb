09adbe8dff7625af12561637e4f2b3075b5529d4
liujialing <liujialing@b.360.com>
2017-10-26 14:40:23 +0800

    add merge_sort_bu

--- sort/sort.rb
+++ sort/sort.rb
@@ -78,7 +78,8 @@ class Sort
       end
       #if I change tmp into arr[i],the sort is worry.because the position is change
       arr[j]=tmp
-    end 
+    end
+    arr 
   end  
 
   #希尔排序
@@ -104,13 +105,33 @@ class Sort
    merge_sort_recursion(arr,l,r)
   end  
 
+  #自底向上的归并排序
+  def merge_sort_bu(arr)
+    size=1
+    while(size<arr.size)
+      i=0
+      while(i+size<arr.size)
+        if(size<16)
+          arr[i..(i+size+size-1)]=optimize_insert_sort(arr[i..(i+size+size-1)])
+        else(arr[i+size-1]>arr[i+size])
+          merge_array(arr,i,i+size-1,[i+size*2-1,arr.size-1].min)
+        end  
+        i+=size*2
+      end  
+      size *= 2
+    end 
+  end  
+
   private 
   def merge_sort_recursion(arr,l,r)
-    return optimize_insert_sort(arr[l..r]) if r-l<=15
+    if r-l<=15
+      arr[l..r]=optimize_insert_sort(arr[l..r]) 
+      return arr[l..r]
+    end
     mid = (l+r)/2
     merge_sort_recursion(arr,l,mid)
     merge_sort_recursion(arr,mid+1,r)
-    if arr[mid]<arr[mid+1]
+    if arr[mid]>arr[mid+1]
       merge_array(arr,l,mid,r)
     end  
   end
--- sort/test_sort.rb
+++ sort/test_sort.rb
@@ -2,21 +2,20 @@
 require_relative 'sort'
 require_relative 'sort_helper'
 
-=begin
-array=SortHelper::generate_array(10000,0,9999)
+# array=SortHelper::generate_array(10000,0,9999)
 
-sort_array=["bubble_sort","optimize_bubble_sort","select_sort","insert_sort","optimize_insert_sort","shell_insert","merge_sort"]
+# sort_array=["bubble_sort","optimize_bubble_sort","select_sort","insert_sort","optimize_insert_sort","shell_insert","merge_sort"]
 
-sort_array.each do |method_name|
-  SortHelper::caculate_time(method_name,array)
-end  
-=end
+# sort_array.each do |method_name|
+#   SortHelper::caculate_time(method_name,array)
+# end  
 
-array1=SortHelper::generate_nearly_order_array(100000,10)
-sort_nearly_order_array = ["optimize_insert_sort","merge_sort"]
+#array1=SortHelper::generate_nearly_order_array(100000,10)
 
+sort_nearly_order_array = ["optimize_insert_sort","merge_sort","merge_sort_bu"]
+array=SortHelper::generate_array(50,0,99)
 sort_nearly_order_array.each do |method_name|
-  SortHelper::caculate_time(method_name,array1)
+  SortHelper::caculate_time(method_name,array)
 end 
 
 
